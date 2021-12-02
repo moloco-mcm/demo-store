68bdbb55d7940402102d8a1b69df4f5b3cbfd9c1
Sang Jun Han <sjhan@molocoads.com>
2021-12-02 11:30:18 -0800

    Fix wrong path for i18n locale data

--- packages/seller-portal/next-i18next.config.js
+++ packages/seller-portal/next-i18next.config.js
@@ -8,6 +8,6 @@ module.exports = {
   // Use a reserved env variable name for AWS Lambda to check if the server is running in Netlify production env
   // https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
   localePath: process.env.AWS_LAMBDA_FUNCTION_NAME
-    ? path.resolve('./packages/demo-store-seller-portal/src/assets/i18n') // use full path when the app is running in Netlify function env
+    ? path.resolve('./packages/seller-portal/src/assets/i18n') // use full path when the app is running in Netlify function env
     : path.resolve('./src/assets/i18n'),
 };
