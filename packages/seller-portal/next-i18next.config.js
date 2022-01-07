702f502a57efe7348b0b4f60fa8a80be9755195d
Sang Jun Han <sjhan@molocoads.com>
2022-01-07 10:54:26 -0800

    remove Netlify specific i18n asset loading

--- packages/seller-portal/next-i18next.config.js
+++ packages/seller-portal/next-i18next.config.js
@@ -5,9 +5,5 @@ module.exports = {
     locales: ['en', 'ko'],
     defaultLocale: 'en',
   },
-  // Use a reserved env variable name for AWS Lambda to check if the server is running in Netlify production env
-  // https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
-  localePath: process.env.AWS_LAMBDA_FUNCTION_NAME
-    ? path.resolve('./packages/seller-portal/src/assets/i18n') // use full path when the app is running in Netlify function env
-    : path.resolve('./src/assets/i18n'),
+  localePath: path.resolve('./src/assets/i18n'),
 };
