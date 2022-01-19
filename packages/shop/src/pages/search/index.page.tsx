ddcd477beb62f7a6a86e01b62e9ab61d44f6fb7e
Sang Jun Han <sjhan@molocoads.com>
2022-01-18 18:56:41 -0800

    debounce search input

--- packages/shop/src/pages/search/index.page.tsx
+++ packages/shop/src/pages/search/index.page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import 'styled-components/macro';
+import debounce from 'lodash/debounce';
 
 import Input from '@rmp-demo-store/ui/input';
 import Button from '@rmp-demo-store/ui/button';
@@ -11,13 +12,34 @@ import { space, zIndex } from '@rmp-demo-store/ui/theme-utils';
 import AppLayout from '../../containers/app-layout';
 import SearchResult from '../../containers/search-result';
 
+const DEBOUNCE_TIME = 1_000;
+
 const Search: NextPage<{}> = () => {
   const [searchWord, setSearchWord] = React.useState('');
+  const [debouncedSearchWord, setDebouncedSearchWord] =
+    React.useState(searchWord);
+
+  const updateDebouncedSearchWord = React.useMemo(
+    () =>
+      debounce((newSearchWord: string) => {
+        setDebouncedSearchWord(newSearchWord);
+      }, DEBOUNCE_TIME),
+    []
+  );
 
   const handleSearchWordInputChange: React.ChangeEventHandler<
     HTMLInputElement
   > = (e) => {
-    setSearchWord(e.target.value);
+    const { value } = e.target;
+    setSearchWord(value);
+    updateDebouncedSearchWord(value.trim());
+  };
+
+  const handleSearchWordInputKeyDown: React.KeyboardEventHandler<
+    HTMLInputElement
+  > = (e) => {
+    if (e.key !== 'Enter') return;
+    updateDebouncedSearchWord.flush();
   };
 
   return (
@@ -37,13 +59,15 @@ const Search: NextPage<{}> = () => {
           `}
         >
           <Input
+            type="text"
             value={searchWord}
             autoFocus
             onChange={handleSearchWordInputChange}
+            onKeyDown={handleSearchWordInputKeyDown}
           />
           <Button variant="ghost">Cancel</Button>
         </Stack>
-        <SearchResult searchWord={searchWord} />
+        <SearchResult searchWord={debouncedSearchWord} />
       </AppLayout>
     </>
   );
