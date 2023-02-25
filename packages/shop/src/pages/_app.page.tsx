39b0a7845e04373c98f6ff33e2a4cd746ef528b3
Sang Jun Han <sjhan@molocoads.com>
2023-02-24 18:15:54 -0800

    add GTM

--- packages/shop/src/pages/_app.page.tsx
+++ packages/shop/src/pages/_app.page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider } from 'styled-components/macro';
 // manual importing fontawesome css prevents flashing unstyled fontawesome icon
 // ref: https://github.com/FortAwesome/react-fontawesome/issues/234
@@ -17,6 +18,7 @@ import GlobalStyle from '@rmp-demo-store/ui/global-style';
 import CSSReset from '@rmp-demo-store/ui/css-reset';
 
 import '../common/i18n';
+import { GTM_ID } from '../common/gtm';
 
 if (process.env.NODE_ENV !== 'development') {
   // send react-query logs to sentry
@@ -42,15 +44,33 @@ function MyApp({ Component, pageProps }: AppProps) {
   });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <ThemeProvider theme={theme}>
-          <CSSReset />
-          <GlobalStyle baseFontSize={16} />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      {/* Google Tag Manager */}
+      {GTM_ID ? (
+        <Script
+          id="gtag-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+          }}
+        />
+      ) : null}
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemeProvider theme={theme}>
+            <CSSReset />
+            <GlobalStyle baseFontSize={16} />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
 export default MyApp;
