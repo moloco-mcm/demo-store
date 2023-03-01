7d12afcc2b78e935b29cd3704ee48b2e842f568f
Sang Jun Han <sjhan@molocoads.com>
2023-02-28 17:00:18 -0800

    extract gtm script into GoogleTagManager

--- packages/shop/src/pages/_app.page.tsx
+++ packages/shop/src/pages/_app.page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 import { ThemeProvider } from 'styled-components/macro';
 // manual importing fontawesome css prevents flashing unstyled fontawesome icon
 // ref: https://github.com/FortAwesome/react-fontawesome/issues/234
@@ -19,6 +18,7 @@ import CSSReset from '@rmp-demo-store/ui/css-reset';
 
 import '../common/i18n';
 import { GTM_ID } from '../common/gtm';
+import { GoogleTagManager } from '../components/common/google-tag-manager';
 
 if (process.env.NODE_ENV !== 'development') {
   // send react-query logs to sentry
@@ -45,22 +45,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {/* Google Tag Manager */}
-      {GTM_ID ? (
-        <Script
-          id="gtag-base"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
-          `,
-          }}
-        />
-      ) : null}
+      {GTM_ID ? <GoogleTagManager gtmId={GTM_ID} /> : null}
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <ThemeProvider theme={theme}>
